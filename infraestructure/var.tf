6c46f4694a7c4caed9e288ab5bf21f857a1cdc11
Xavier Méndez <shavy_00@hotmail.com>
2020-02-07 09:26:47 -0600

    Adding variables

--- infraestructure/var.tf
+++ infraestructure/var.tf
@@ -1,1 +1,20 @@
 
+variable "aws_region" {
+  description   = "AWS Region"
+  default     = ""
+}
+
+variable "env" {
+  type   = "string"
+  default     = "Course"
+}
+
+variable "product" {
+  type   = "string"
+  default     = ""
+}
+
+variable "dir_type" {
+  type   = "string"
+  default     = "SimpleAD"
+}
