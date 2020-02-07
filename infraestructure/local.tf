5151faa5d64d06263b581752bd3a80f360067e4a
Xavier Méndez <shavy_00@hotmail.com>
2020-02-07 08:45:15 -0600

    Updating local variables

--- infraestructure/local.tf
+++ infraestructure/local.tf
@@ -1,1 +1,24 @@
-
+locals{
+  name_prefix = var.env
+  
+  default_tags = {
+    Environmevarnt  = var.env
+    Product         = var.product
+  }
+  
+  asg_default_tags = 
+  [
+    {
+      key       = "Name",
+      value     = "APP-ASG",
+      propagate_at_launch = ""true
+      
+    },
+    {
+      key       = "Environmnet",
+      value     = var.env,
+      propagate_at_launch = ""true
+      
+    }
+  ]
+}
