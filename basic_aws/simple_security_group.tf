c85c1c9940c67629232092fe8eb2b0ce72ab898d
Xavier Méndez <shavy_00@hotmail.com>
2020-02-27 17:14:04 -0600

    Adding security group

---
+++ basic_aws/simple_security_group.tf
@@ -0,0 +1,36 @@
+# security groups
+
+resource "aws_security_group" "sg_main_1" {
+  name        = "sg_allow_ping_ssh"
+  description = "Allow ping ssh"
+  vpc_id      = "${aws_vpc.vpc_main_1.id}"
+
+  #ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #ping
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "sg_main_1"
+  }
+
+}
