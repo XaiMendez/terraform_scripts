d95453cd2417c4dad298d528a3c52438fc142ae9
Xavier Méndez <shavy_00@hotmail.com>
2020-03-12 17:09:23 -0600

    updating vpc

--- basic_aws/simple_vpc.tf
+++ basic_aws/simple_vpc.tf
@@ -1,8 +1,12 @@
-# vpc
+# simple_vpc.tf
+
+# region
 provider "aws" {
   region = "us-east-2"
 }
 
+
+# vpc vpc_main_1
 resource "aws_vpc" "vpc_main_1" {
   cidr_block = "10.0.0.0/16"
   enabled_dns_hostnames = true
